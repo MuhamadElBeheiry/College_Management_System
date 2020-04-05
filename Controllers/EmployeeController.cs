f9e635c16ccc84caabee41ee51b5c100c4ca4ebf
Mohamed Ahmed Saad Ahmed <Mohamedahmedsaadahmed77@gmail.com>
2020-04-05 23:13:07 +0200

    Initial Structure

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class StudentController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+    }
+}
