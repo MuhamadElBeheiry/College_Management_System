15f0d3c7a4066afcf32d19ab8358dd6ceb231dd1
mohamed10111213 <ma01122555797@gmail.com>
2020-04-06 17:21:38 +0200

    Add student with view concatination

--- Controllers/DeanController.cs
+++ Controllers/DeanController.cs
@@ -10,5 +10,22 @@ namespace Project.Controllers
     public class DeanController : Controller
     {
         private DatabaseContext db = new DatabaseContext();
+        public ActionResult Index()
+        {
+            ViewData["Departments"] = db.Departments.ToList();
+            ViewData["Levels"] = db.Levels.ToList();
+            ViewData["Students"] = db.Students.ToList();
+            ViewData["Doctors"] = db.Doctors.ToList();
+            ViewData["Employees"] = db.Employees.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
